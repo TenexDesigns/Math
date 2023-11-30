0044804e565b517c59bea5bf206cf282139832c2
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-12-01 02:10:30 +0300

    Update Coordinate System is ALgebra + Georgapy.java

--- Coordinate System is ALgebra + Georgapy.java
+++ Coordinate System is ALgebra + Georgapy.java
@@ -1,1 +1,8 @@
 kim..
+
+
+  descrates analytic geormetr - analyze curver as famly of equations - solved parboloc
+
+    descrates method - also solved showing relationship between two varables, e.g cabages and prices or line graph
+
+    analytic geormetr
