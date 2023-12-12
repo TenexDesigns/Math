1ad52657d61dc049429a1eef00b5feb3c41797ea
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-12-12 18:08:14 +0300

    Create 2_Trigonometry defined.java

---
+++ Trigonometry/1_Trigonometry defined/2_Trigonometry defined.java
@@ -0,0 +1,22 @@
+Trigonometry is a branch of mathematics that deals with the relationships between the angles and sides of triangles. The name "trigonometry" comes from the Greek words "trigonon" (meaning "triangle") and "metron" (meaning "to measure") [Source 4](https://www.britannica.com/science/trigonometry).
+
+The main functions in trigonometry are sine (sin), cosine (cos), and tangent (tan). These functions are derived from the ratios of the sides of a right-angled triangle [Source 0](https://www.mathsisfun.com/algebra/trigonometry.html).
+
+- **Sine (sin)**: The sine of an angle in a right triangle is the ratio of the length of the side opposite the angle to the length of the hypotenuse. It can also be defined in terms of the unit circle as the y-coordinate of the point where the angle intersects the circle [Source 0](https://www.mathsisfun.com/algebra/trigonometry.html).
+
+- **Cosine (cos)**: The cosine of an angle in a right triangle is the ratio of the length of the side adjacent to the angle to the length of the hypotenuse. In terms of the unit circle, it is the x-coordinate of the point where the angle intersects the circle [Source 0](https://www.mathsisfun.com/algebra/trigonometry.html).
+
+- **Tangent (tan)**: The tangent of an angle in a right triangle is the ratio of the sine to the cosine of that angle. In terms of the unit circle, it is the ratio of the y-coordinate to the x-coordinate of the point where the angle intersects the circle [Source 0](https://www.mathsisfun.com/algebra/trigonometry.html).
+
+Trigonometric functions are used in a variety of applications, including science, engineering, and video games. They help us find angles and distances, and are used to solve problems involving angles and distances in geometric figures [Source 0](https://www.mathsisfun.com/algebra/trigonometry.html).
+
+For example, if you know the length of the hypotenuse and the angle, you can find the length of the other side using the sine function. If you know the lengths of two sides and the included angle, you can find the length of the hypotenuse using the cosine function. And if you know the lengths of two sides and the included angle, you can find the length of the other side using the tangent function [Source 0](https://www.mathsisfun.com/algebra/trigonometry.html).
+
+In addition to these basic functions, there are also inverse trigonometric functions, which allow you to find the angle given the ratio of two sides of a right triangle. These are often written as sin^-1, cos^-1, or tan^-1, and are often represented as asin, acos, and atan in programming languages [Source 1](https://betterexplained.com/articles/intuitive-trigonometry/).
+
+Trigonometry is not limited to right triangles. It can also be applied to other types of polygons using the Law of Sines and the Law of Cosines. The Law of Sines states that the ratio of the length of a side of a triangle to the sine of the angle opposite that side is the same for all three sides. The Law of Cosines states that the square of the length of a side of a triangle is equal to the sum of the squares of the lengths of the other two sides minus twice the product of those two sides and the cosine of the angle between them [Source 0](https://www.mathsisfun.com/algebra/trigonometry.html).
+
+In summary, trigonometry is a powerful tool for dealing with angles and distances in various fields, and it provides a systematic way to calculate and understand these relationships.
+
+
+  ....
