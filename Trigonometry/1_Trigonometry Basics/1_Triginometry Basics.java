29f8a2701b41ece1ab2fe32bff060435831b6f62
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-12-13 21:20:48 +0300

    Create 1_Triginometry Basics.java

---
+++ Trigonometry/1_Trigonometry Basics/1_Triginometry Basics.java
@@ -0,0 +1,133 @@
+Trigonometry
+Trigonometry is the branch of mathematics that deals with the relationship between ratios of the sides of a right-angled triangle with its angles. The ratios used to study this relationship are called trigonometric ratios, namely, sine, cosine, tangent, cotangent, secant, cosecant. The word trigonometry is a 16th century Latin derivative and the concept was given by the Greek mathematician Hipparchus.
+
+Here in the below content, we shall understand the basics of trigonometry, the various identities-formulas of trigonometry, and the real-life examples or applications of trigonometry.
+
+Introduction to Trigonometry
+Trigonometry is one of the most important branches in mathematics. The word trigonometry is formed by clubbing words 'Trigonon' and 'Metron' which means triangle and measure respectively. It is the study of the relation between the sides and angles of a right-angled triangle. It thus helps in finding the measure of unknown dimensions of a right-angled triangle using formulas and identities based on this relationship.
+
+introduction to the term trigonometry
+
+Trigonometry Basics
+Trigonometry basics deal with the measurement of angles and problems related to angles. There are three basic functions in trigonometry: sine, cosine, and tangent. These three basic ratios or functions can be used to derive other important trigonometric functions: cotangent, secant, and cosecant. All the important concepts covered under trigonometry are based on these functions. Hence, further, we need to learn these functions and their respective formulas at first to understand trigonometry.
+
+right angled triangle
+
+In a right-angled triangle, we have the following three sides.
+
+Perpendicular - It is the side opposite to the angle θ.
+Base - This is the adjacent side to the angle θ.
+Hypotenuse - This is the side opposite to the right angle.
+
+Trigonometric Ratios
+There are basic six ratios in trigonometry that help in establishing a relationship between the ratio of sides of a right triangle with the angle. If θ is the angle in a right-angled triangle, formed between the base and hypotenuse, then
+
+sin θ = Perpendicular/Hypotenuse
+cos θ = Base/Hypotenuse
+tan θ = Perpendicular/Base
+The value of the other three functions: cot, sec, and cosec depend on tan, cos, and sin respectively as given below.
+
+cot θ = 1/tan θ = Base/Perpendicualr
+sec θ = 1/cos θ = Hypotenuse/Base
+cosec θ = 1/sin θ = Hypotenuse/Perpendicular
+  
+Trigonometric Table
+The trigonometric table is made up of trigonometric ratios that are interrelated to each other – sine, cosine, tangent, cosecant, secant, cotangent. 
+  These ratios, in short, are written as sin, cos, tan, cosec, sec, cot, and are taken for standard angle values. 
+  You can refer to the trigonometric table chart to know more about these ratios.
+
+trigonometric table
+
+Important Trigonometric Angles
+Trigonometric angles are the angles in a right-angled triangle using which different trigonometric functions can be represented.
+  Some standard angles used in trigonometry are 0º, 30º, 45º, 60º, 90º. The trigonometric values for these angles can be observed directly in a trigonometric table.
+  Some other important angles in trigonometry are 180º, 270º, and 360º. Trigonometry angle can be expressed in terms of trigonometric ratios as,
+
+θ = sin-1 (Perpendicular/Hypotenuse)
+θ = cos-1 (Base/Hypotenuse)
+θ = tan-1 (Perpendicular/Base)
+
+
+  
+List of Trigonometric Formulas
+There are different formulas in trigonometry depicting the relationships between trigonometric ratios and the angles for different quadrants. The basic trigonometry formulas list is given below:
+
+1. Trigonometry Ratio Formulas
+
+sin θ = Opposite Side/Hypotenuse
+cos θ = Adjacent Side/Hypotenuse
+tan θ = Opposite Side/Adjacent Side
+cot θ = 1/tan θ = Adjacent Side/Opposite Side
+sec θ = 1/cos θ = Hypotenuse/Adjacent Side
+cosec θ = 1/sin θ = Hypotenuse/Opposite Side
+2. Trigonometry Formulas Involving Pythagorean Identities
+
+sin²θ + cos²θ = 1
+tan2θ + 1 = sec2θ
+cot2θ + 1 = cosec2θ
+3. Sine and Cosine Law in Trigonometry
+
+a/sinA = b/sinB = c/sinC
+c2 = a2 + b2 – 2ab cos C
+a2 = b2 + c2 – 2bc cos A
+b2 = a2 + c2 – 2ac cos B
+  
+Here a, b, c are the lengths of the sides of the triangle and A, B, and C are the angle of the triangle.
+
+The complete list of trigonometric formulas involving trigonometry ratios and trigonometry identities is listed for easy access. Here's a list of all the trigonometric formulas for you to learn and revise.
+
+Trigonometric Functions Graphs
+Different properties of a trigonometric function like domain, range, etc can be studied using the trigonometric function graphs. The graphs of basic trigonometric functions- Sine and Cosine are given below:
+
+graph of trigonometric functions
+
+The domain and range of sin and cosine functions can thus be given as,
+
+sin θ: Domain (-∞, + ∞); Range [-1, +1]
+cos θ: Domain (-∞ +∞); Range [-1, +1]
+Click here to learn about the graphs of all trigonometric functions and their domain and range in detail- Trigonometric Functions
+
+Unit Circle and Trigonometric Values
+Unit circle can be used to calculate the values of basic trigonometric functions- sine, cosine, and tangent. The following diagram shows how trigonometric ratios sine and cosine can be represented in a unit circle.
+
+Unit Circle - Trigonometric Ratios
+
+Trigonometry Identities
+In Trigonometric Identities, an equation is called an identity when it is true for all values of the variables involved. Similarly, an equation involving trigonometric ratios of an angle is called a trigonometric identity, if it is true for all values of the angles involved. In trigonometric identities, you will get to learn more about the Sum and Difference Identities.
+
+For example, sin θ/cos θ = [Opposite/Hypotenuse] ÷ [Adjacent/Hypotenuse] = Opposite/Adjacent = tan θ
+
+Therefore, tan θ = sin θ/cos θ is a trigonometric identity. The three important trigonometric identities are:
+
+sin²θ + cos²θ = 1
+tan²θ + 1 = sec²θ
+cot²θ + 1 = cosec²θ
+Applications of Trigonometry
+Throughout history, trigonometry has been applied in areas such as architecture, celestial mechanics, surveying, etc. Its applications include in:
+
+Various fields like oceanography, seismology, meteorology, physical sciences, astronomy, acoustics, navigation, electronics, and many more.
+It is also helpful to find the distance of long rivers, measure the height of the mountain, etc.
+Spherical trigonometry has been used for locating solar, lunar, and stellar positions.
+Real-Life Examples of Trigonometry
+Trigonometry has many real-life examples used broadly. Let’s get a better idea of trigonometry with an example. A boy is standing near a tree. He looks up at the tree and wonders “How tall is the tree?” The height of the tree can be found without actually measuring it. What we have here is a right-angled triangle, i.e., a triangle with one of the angles equal to 90 degrees. Trigonometric formulas can be applied to calculate the height of the tree, if the distance between the tree and boy, and the angle formed when the tree is viewed from the ground is given.
+
+real life trigonometry example of a boy looking at a tree wondering its height
+
+It is determined using the tangent function, such as tan of angle is equal to the ratio of the height of the tree and the distance. Let us say the angle is θ, then
+
+tan θ = Height/Distance between object & tree
+Distance = Height/tan θ
+
+Let us assume that distance is 30m and the angle formed is 45 degrees, then
+
+Height = 30/tan 45°
+Since, tan 45° = 1
+So, Height = 30 m
+
+The height of the tree can be found out by using basic trigonometry formulas.
+
+
+
+
+
+  ...
