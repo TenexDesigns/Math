6fbae77dab2480b142ee2f852f928cbda81d9696
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-12-13 20:12:22 +0300

    Create Spherical vs Hyperbolic vs  Euclidean Geometry.java

---
+++ Geometry/1_Geometry/Euclidean vs. Non-Euclidean Geometry/Spherical vs Hyperbolic vs  Euclidean Geometry.java
@@ -0,0 +1,25 @@
+In Euclidean geometry, which is the familiar geometry of flat surfaces like a piece of paper or a tabletop, the sum of the angles in a triangle is always 180 degrees. This is known as the Euclidean triangle sum theorem.
+
+In non-Euclidean geometries, such as spherical geometry and hyperbolic geometry, the rules change:
+
+### 1. **Spherical Geometry:**
+   - On a sphere, like the Earths surface, triangles do not have angles that add up to 180 degrees. Instead, the sum of the angles in a spherical triangle is always greater than 180 degrees. The exact sum depends on the size of the triangle and is related to the sphere's curvature.
+
+### 2. **Hyperbolic Geometry:**
+   - In hyperbolic geometry, which can be thought of as the geometry of a saddle-shaped surface, triangles have angles that add up to less than 180 degrees. The exact sum depends on the size of the triangle and is related to the hyperbolic curvature.
+
+### Circular or Elliptic Geometry:
+   - The term "circular geometry" is not commonly used, but it might be referring to elliptic geometry, which is another name for spherical geometry. In elliptic geometry, triangles also have angles that add up to more than 180 degrees.
+
+### Key Points:
+- **Euclidean Geometry:** Triangles have angles adding up to 180 degrees on a flat plane.
+- **Spherical (Elliptic) Geometry:** Triangles have angles adding up to more than 180 degrees on a sphere.
+- **Hyperbolic Geometry:** Triangles have angles adding up to less than 180 degrees on a hyperbolic surface.
+
+These different geometries arise from different assumptions about parallel lines and the nature of space. Euclidean geometry assumes parallel lines remain at a constant distance from each other, while non-Euclidean geometries allow for different behaviors of parallel lines.
+
+These concepts expand our understanding of geometry beyond the classical Euclidean model and have applications in various branches of mathematics, physics, and even in the study of curved spacetime in general relativity.
+
+
+
+  ...
