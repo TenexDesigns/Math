e8a083c32d030edb561669db84ebb521ad5132b6
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-12-15 10:26:47 +0300

    Create List 2.java

---
+++ Trigonometry/Trigonometric identities/List 2.java
@@ -0,0 +1,50 @@
+Here's a list of some important trigonometric identities, categorized by type:
+
+**Fundamental Identities:**
+
+* **Pythagorean Theorem:** sin^2(θ) + cos^2(θ) = 1
+* **Tangent Identity:** tan(θ) = sin(θ) / cos(θ)
+* **Reciprocal Identities:**
+    * sin(θ) = 1 / csc(θ)
+    * cos(θ) = 1 / sec(θ)
+    * tan(θ) = 1 / cot(θ)
+
+**Angle Addition and Subtraction:**
+
+* sin(θ + φ) = sin(θ)cos(φ) + cos(θ)sin(φ)
+* cos(θ + φ) = cos(θ)cos(φ) - sin(θ)sin(φ)
+* tan(θ + φ) = (tan(θ) + tan(φ)) / (1 - tan(θ)tan(φ))
+* sin(θ - φ) = sin(θ)cos(φ) - cos(θ)sin(φ)
+* cos(θ - φ) = cos(θ)cos(φ) + sin(θ)sin(φ)
+* tan(θ - φ) = (tan(θ) - tan(φ)) / (1 + tan(θ)tan(φ))
+
+**Double Angle and Half Angle:**
+
+* sin(2θ) = 2sin(θ)cos(θ)
+* cos(2θ) = cos^2(θ) - sin^2(θ) = 2cos^2(θ) - 1 = 1 - 2sin^2(θ)
+* tan(2θ) = 2tan(θ) / (1 - tan^2(θ))
+* sin(θ/2) = ±√((1 - cos(θ)) / 2)
+* cos(θ/2) = ±√((1 + cos(θ)) / 2)
+* tan(θ/2) = ±√((1 - cos(θ)) / (1 + cos(θ)))
+
+**Product-to-Sum and Sum-to-Product:**
+
+* sin(α)sin(β) = (1/2)(cos(α - β) - cos(α + β))
+* cos(α)cos(β) = (1/2)(cos(α + β) + cos(α - β))
+* sin(α)cos(β) = (1/2)(sin(α + β) + sin(α - β))
+
+**Other Useful Identities:**
+
+* sin(-θ) = -sin(θ)
+* cos(-θ) = cos(θ)
+* tan(-θ) = -tan(θ)
+* sin(π - θ) = sin(θ)
+* cos(π - θ) = -cos(θ)
+* tan(π - θ) = -tan(θ)
+
+This list provides a starting point, and many other identities exist based on specific contexts and applications. Remember, the key to using these identities effectively is understanding their logic and recognizing patterns in expressions to apply them appropriately.
+
+Do you have any specific identities you'd like to know more about, or a particular area of application you're interested in? I'm happy to delve deeper!
+
+
+  
