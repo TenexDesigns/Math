0e34a03dba5d63eda43834eaead50d8551a4683f
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-12-15 10:24:54 +0300

    Create 1_Trigonometric identities.java

---
+++ Trigonometry/Trigonometric identities/1_Trigonometric identities.java
@@ -0,0 +1,47 @@
+Trigonometric identities are equalities that involve trigonometric functions and are true for every value of the occurring variables for which both sides of the equality are defined. They are useful whenever expressions involving trigonometric functions need to be simplified. Here are some of the standard trigonometric identities:
+
+1. **Pythagorean Identities**:
+  - `sin^2(θ) + cos^2(θ) = 1`
+  - `1 + cot^2(θ) = csc^2(θ)`
+  - `tan^2(θ) + 1 = sec^2(θ)`
+  - `sin^2(θ) = (1 - cos^2(θ))`
+  - `cos^2(θ) = (1 - sin^2(θ))`
+  - `sec^2(θ) - 1 = tan^2(θ)`
+  - `csc^2(θ) - 1 = cot^2(θ)` [Source 0](https://en.wikipedia.org/wiki/List_of_trigonometric_identities)
+
+2. **Sum and Difference Identities**:
+  - `sin(θ + φ) = sin(θ)cos(φ) + cos(θ)sin(φ)`
+  - `cos(θ + φ) = cos(θ)cos(φ) - sin(θ)sin(φ)`
+  - `sin(θ - φ) = sin(θ)cos(φ) - cos(θ)sin(φ)`
+  - `cos(θ - φ) = cos(θ)cos(φ) + sin(θ)sin(φ)` [Source 0](https://en.wikipedia.org/wiki/List_of_trigonometric_identities)
+
+3. **Double Angle Identities**:
+  - `sin(2θ) = 2sin(θ)cos(θ)`
+  - `cos(2θ) = cos^2(θ) - sin^2(θ) = 2cos^2(θ) - 1 = 1 - 2sin^2(θ)`
+  - `tan(2θ) = 2tan(θ)/(1 - tan^2(θ))` [Source 0](https://en.wikipedia.org/wiki/List_of_trigonometric_identities)
+
+4. **Half Angle Identities**:
+  - `sin(θ/2) = ±sqrt((1 - cos(θ))/2)`
+  - `cos(θ/2) = ±sqrt((1 + cos(θ))/2)`
+  - `tan(θ/2) = ±sqrt((1 - cos(θ))/(1 + cos(θ)))` [Source 0](https://en.wikipedia.org/wiki/List_of_trigonometric_identities)
+
+5. **Inverse Trigonometric Identities**:
+  - `sin^2(θ) = 1 - cos^2(θ)`
+  - `cos^2(θ) = 1 - sin^2(θ)`
+  - `sec^2(θ) - 1 = tan^2(θ)`
+  - `csc^2(θ) - 1 = cot^2(θ)`
+  - `1 + cot^2(θ) = csc^2(θ)`
+  - `1 + tan^2(θ) = sec^2(θ)` [Source 0](https://en.wikipedia.org/wiki/List_of_trigonometric_identities)
+
+6. **Quotient Identities**:
+  - `cot(θ) = 1/tan(θ)`
+  - `csc(θ) = 1/sin(θ)`
+  - `sec(θ) = 1/cos(θ)` [Source 0](https://en.wikipedia.org/wiki/List_of_trigonometric_identities)
+
+These identities can be used to simplify expressions involving trigonometric functions, and they are fundamental in many areas of mathematics and physics.
+
+
+
+
+
+  .....
